837459b632e878af1d71af313448e4ebe722c19b
KUNTHEA.YON <KUNTHEA.YON@PNC.LOCAL>
2026-07-14 11:15:16 +0700

    #47 update_implement create_leave request api

--- app/Http/Requests/UpdateLeaveRequest.php
+++ app/Http/Requests/UpdateLeaveRequest.php
@@ -25,4 +25,18 @@ class UpdateLeaveRequest extends FormRequest
             'reason' => ['sometimes', 'string', 'min:5', 'max:500'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'leave_type_id.required' => 'Please select a leave type.',
+            'leave_type_id.exists' => 'Selected leave type does not exist.',
+            'start_date.required' => 'Start date is required.',
+            'start_date.after_or_equal' => 'Start date cannot be in the past.',
+            'end_date.required' => 'End date is required.',
+            'end_date.after_or_equal' => 'End date cannot be before start date.',
+            'reason.required' => 'Please provide a reason for your leave request.',
+            'reason.min' => 'Reason must be at least 5 characters.',
+        ];
+    }
 }
